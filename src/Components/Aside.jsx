cd59691604730b69aebad8554fef042238ea2c8b
saheer07 <saheerchungat07@gmail.com>
2025-05-23 16:23:05 +0530

    Seond Commit

--- src/Components/Aside.jsx
+++ src/Components/Aside.jsx
@@ -8,9 +8,9 @@ function Aside({ color, setColor }) {
 
   return (
     <div className="border-2 border-green-500 p-4 rounded-md shadow-sm flex flex-col gap-3 items-start">
-      <h2 className="text-lg font-semibold" style={{ color }}>
+      <h1 className="text-lg font-semibold" style={{ color }}>
         Aside
-      </h2>
+      </h1>
       <button
         onClick={changeColor}
         className="bg-purple-400 text-bold px-4 py-2 rounded hover:bg-purple-600" style={{color}}
