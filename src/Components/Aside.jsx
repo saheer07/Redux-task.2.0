c438a26be834ef400c2093d6fafb4997d397bcd2
saheer07 <saheerchungat07@gmail.com>
2025-05-23 16:15:41 +0530

    .

--- src/Components/Aside.jsx
+++ src/Components/Aside.jsx
@@ -13,9 +13,9 @@ function Aside({ color, setColor }) {
       </h2>
       <button
         onClick={changeColor}
-        className="bg-purple-500 text-white px-4 py-2 rounded hover:bg-purple-600" style={{color}}
+        className="bg-purple-400 text-bold px-4 py-2 rounded hover:bg-purple-600" style={{color}}
       >
-        Change Color
+        Change Col
       </button>
     </div>
   );
